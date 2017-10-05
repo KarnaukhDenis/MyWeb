9d0868293bf2610d9872f0803546ea4b48abf87d
Karnaukh Denis <LV426D@mail.ru>
2017-10-05 21:41:44 +1000

    New site

---
+++ js.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+	document.querySelector('#menu').onmouseover = menuShow;
+	document.querySelector('#menu').onmouseout = menuHide;
+	document.onkeypdown = function(event) {
+		console.log(event);
+		if (event.code == 'KeyM') menuShow();
+	}
+	function menuShow() {
+		document.querySelector('#menu').style.left = 0;
+		}
+	function menuHide() {
+		document.querySelector('#menu').style.left = '-230px';
+		}
+}
